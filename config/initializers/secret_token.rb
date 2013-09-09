6d6d61ab2d3f06954a1b7bde2b73383c555ef55c
Alvis S. Hall <alvis.cert.hall@gmail.com>
2013-09-08 23:51:08 -0500

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Voicetree2::Application.config.secret_token = 'ef7726f6107dc4b337439765919484d41b2e75b03a920b58dee3b0e5ba58aede07d5f2aceaac98bf364186c2c1a6fa9144de0cc879ae23e9de4294632fea0322'
